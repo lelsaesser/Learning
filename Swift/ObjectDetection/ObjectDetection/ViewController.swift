c4514835707d32d0ca486aae734fed8b74cde781
Lucas Elsässer <lucas.elsaesser@gmail.com>
2019-05-27 09:35:08 +0200

    added label with detected identifier and confidence

--- Swift/ObjectDetection/ObjectDetection/ViewController.swift
+++ Swift/ObjectDetection/ObjectDetection/ViewController.swift
@@ -12,6 +12,10 @@ import Vision
 import ARKit
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    private var detectedLabel = "123"
+    private var detectedLabelConfidence = VNConfidence()
+    private var textView = UILabel(frame: CGRect(x: 20, y: 50, width: 300, height: 40))
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +37,11 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         let dataOutput = AVCaptureVideoDataOutput()
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(dataOutput)
+        
+        
+        // text field with outputs
+        self.textView.font = UIFont.systemFont(ofSize: 15)
+        self.view.addSubview(textView)
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
@@ -45,10 +54,21 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
             guard let firstObservation = results.first else { return }
             
+            self.detectedLabel = firstObservation.identifier
+            self.detectedLabelConfidence = firstObservation.confidence
+            
             print(firstObservation.identifier, firstObservation.confidence)
+            self.updateTextView()
         }
         
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
+    func updateTextView() {
+        DispatchQueue.main.async {
+            let confidence = String(format: "%.2f", self.detectedLabelConfidence * 100.00)
+            self.textView.text = "\(self.detectedLabel) \(confidence) %"
+        }
     }
 }
 
